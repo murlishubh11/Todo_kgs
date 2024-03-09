d7255725b839638981ddf6725cffe683b2512059
Murlidhar Mewada <murlidhar1105@gmail.com>
2024-03-09 14:58:17 +0530

    updated backend part

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,22 @@
-import express from "express" ;
-import apiRoute from "./utils/api.js";
+import express from "express";
+const app = express();
+import ("./database/conn.js");
+//const cors = require("cors");
+// const path = require("path");
+// const auth = require("./routes/auth");
+// const list = require("./routes/list");
+// app.use(express.json());
+// app.use(cors());
 
-const app  = express();
+// app.use("/api/v1", auth);
+// app.use("/api/v2", list);
 
-const PORT = 8000 ;
+app.get("/", (req, res) => {
+    res.send("Hello");
+//  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+//  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
-app.use('/api/' , apiRoute) ;
-app.listen(PORT , ()=> console.log('server is running at' ,PORT));
-
+app.listen(1000, () => {
+  console.log("Server Started");
+});
